2eaf6574c890f9e0a2b89459f4bd8d9487401465
Beef63 Beef63 <lesnebrice@gmail.com>
2019-01-30 23:03:12 +0100

    lvl3 with electron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    file: null,
+    entry: 0,
+    fileName: ""
+  };
+
+  handleFile = event => {
+    let file = event.target.files[0];
+    if (file) {
+      this.setState({ file: file, fileName: event.target.files[0].name });
+    } else {
+      this.setState({ file: null, fileName: "" });
+    }
+  };
+
+  handleUpload = () => {
+    let file = this.state.file;
+
+    const data = new FormData();
+    data.append("file", file);
+
+    axios
+      .post("https://fhirtest.uhn.ca/baseDstu3/Binary", data)
+      .then(response => {
+        alert("Fichier envoyé avec succés");
+        axios.get("https://fhirtest.uhn.ca/baseDstu3/Binary").then(response => {
+          this.setState({ entry: response.data.entry.length });
+        });
+      })
+      .catch(error => {
+        alert("Une erreure est survenue, veuillez recommencer");
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <div className="header">
+          <img
+            className="logo"
+            src="https://www.lifen.fr/wp-content/themes/lifen_orange/images/lifen-orange.svg"
+            alt="logo"
+          />
+        </div>
+        <div className="container">
+          <div className="btn-choose-send">
+            <label className="btn" for="file">
+              Choisissez un fichier
+            </label>
+            <div>{this.state.fileName}</div>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              className="inputfile"
+              data-multiple-caption="{count} files selected"
+              multiple
+              onChange={this.handleFile}
+            />
+            <button className="btn" onClick={this.handleUpload}>
+              Envoyer
+            </button>
+          </div>
+
+          <label>
+            Nombre total de fichiers enregistrés :{" "}
+            {this.state.entry > 0 && this.state.entry}
+          </label>
+        </div>
+      </section>
     );
   }
 }
